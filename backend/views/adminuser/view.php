de8ad3f372c5e59f379664ae24f9206089cac04b
张笑 <nealse.zh@gmail.com>
2018-05-21 20:40:25 +0800

    first commit

---
+++ backend/views/adminuser/view.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Adminuser */
+
+$this->title = $model->realname;
+$this->params['breadcrumbs'][] = ['label' => '管理员用户', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="adminuser-view">
+    <p>
+        <?= Html::a('更新', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('删除', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => '确认删除这条记录？',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'username',
+            'realname',
+            'email:email',
+            [
+                'attribute' => 'status',
+                'value' => $model->statusStr,
+            ],
+            //'status',
+            [
+                'attribute' => 'created_at',
+                'value' => date('Y-m-d H:i:s', $model->created_at),
+            ],
+        ],
+    ]) ?>
+
+</div>
---
+++ common/models/Adminuser.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "adminuser".
+ *
+ * @property int $id ID
+ * @property string $username 用户名
+ * @property string $realname 姓名
+ * @property string $email 电子邮箱
+ * @property int $status 状态
+ * @property string $password_hash 密码
+ * @property string $auth_key 授权key
+ * @property string $password_reset_token 密码重置token
+ * @property string $access_token 访问token
+ * @property int $expire_at 过期时间
+ * @property int $logged_at 登入时间
+ * @property int $created_at 创建时间
+ * @property int $updated_at 最后修改时间
+ *
+ * @property Article[] $articles
+ */
+class Adminuser extends \yii\db\ActiveRecord implements IdentityInterface
+{
+
+
+    const STATUS_DELETED = 0;
+    const STATUS_ACTIVE = 10;
+
+    public static function allStatus()
+    {
+        return [self::STATUS_ACTIVE=>'正常',self::STATUS_DELETED=>'禁用'];
+    }
+
+    public function getStatusStr()
+    {
+        return $this->status==self::STATUS_ACTIVE?'正常':'禁用';
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'adminuser';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['email', 'password_hash', 'auth_key'], 'required'],
+            [['status', 'expire_at', 'logged_at', 'created_at', 'updated_at'], 'integer'],
+            [['username'], 'string', 'max' => 32],
+            [['realname', 'email', 'password_hash', 'auth_key', 'password_reset_token', 'access_token'], 'string', 'max' => 255],
+            [['access_token'], 'unique'],
+            [['email'], 'unique'],
+            [['username'], 'unique'],
+            [['password_reset_token'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => '用户名',
+            'realname' => '姓名',
+            'email' => '电子邮箱',
+            'status' => '状态',
+            'password_hash' => '密码',
+            'auth_key' => '授权key',
+            'password_reset_token' => '密码重置token',
+            'access_token' => '访问token',
+            'expire_at' => '过期时间',
+            'logged_at' => '登入时间',
+            'created_at' => '创建时间',
+            'updated_at' => '最后修改时间',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticles()
+    {
+        return $this->hasMany(Article::className(), ['created_by' => 'id']);
+    }
+
+    /**
+     * @param bool $insert
+     * @return bool
+     */
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            if ($insert) {
+                $this->created_at = time();
+                $this->updated_at = time();
+            } else {
+                $this->updated_at = time();
+            }
+
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+
+    //------------------------
+
+    /**
+     * @param $password
+     * @throws \yii\base\Exception
+     */
+    public function setPassword($password)
+    {
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+
+    /**
+     * @throws \yii\base\Exception
+     */
+    public function generateAuthKey()
+    {
+        $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+
+    /**
+     * @throws \yii\base\Exception
+     */
+    public function generatePasswordResetToken()
+    {
+        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
+    }
+
+
+    /**
+     *
+     */
+    public function removePasswordResetToken()
+    {
+        $this->password_reset_token = null;
+    }
+
+    /**
+     * Finds an identity by the given ID.
+     * @param string|int $id the ID to be looked for
+     * @return IdentityInterface the identity object that matches the given ID.
+     * Null should be returned if such an identity cannot be found
+     * or the identity is not in an active state (disabled, deleted, etc.)
+     */
+    public static function findIdentity($id)
+    {
+        // TODO: Implement findIdentity() method.
+        return static::findOne(['id' => $id]);
+    }
+
+    /**
+     * Finds an identity by the given token.
+     * @param mixed $token the token to be looked for
+     * @param mixed $type the type of the token. The value of this parameter depends on the implementation.
+     * For example, [[\yii\filters\auth\HttpBearerAuth]] will set this parameter to be `yii\filters\auth\HttpBearerAuth`.
+     * @return IdentityInterface the identity object that matches the given token.
+     * Null should be returned if such an identity cannot be found
+     * or the identity is not in an active state (disabled, deleted, etc.)
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        // TODO: Implement findIdentityByAccessToken() method.
+        return static::findOne(['access_token' => $token]);
+    }
+
+    /**
+     * Returns an ID that can uniquely identify a user identity.
+     * @return string|int an ID that uniquely identifies a user identity.
+     */
+    public function getId()
+    {
+        // TODO: Implement getId() method.
+        return $this->getPrimaryKey();
+    }
+
+    /**
+     * Returns a key that can be used to check the validity of a given identity ID.
+     *
+     * The key should be unique for each individual user, and should be persistent
+     * so that it can be used to check the validity of the user identity.
+     *
+     * The space of such keys should be big enough to defeat potential identity attacks.
+     *
+     * This is required if [[User::enableAutoLogin]] is enabled.
+     * @return string a key that is used to check the validity of a given identity ID.
+     * @see validateAuthKey()
+     */
+    public function getAuthKey()
+    {
+        // TODO: Implement getAuthKey() method.
+        return $this->auth_key;
+    }
+
+    /**
+     * Validates the given auth key.
+     *
+     * This is required if [[User::enableAutoLogin]] is enabled.
+     * @param string $authKey the given auth key
+     * @return bool whether the given auth key is valid.
+     * @see getAuthKey()
+     */
+    public function validateAuthKey($authKey)
+    {
+        // TODO: Implement validateAuthKey() method.
+        return $this->auth_key == $this->getAuthKey();
+    }
+}
